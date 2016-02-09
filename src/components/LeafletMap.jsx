bbe7cffd3b703a237f59593f598f97cd451ccee0
Rui Gil <ruipgil@gmail.com>
2016-02-09 15:39:59 +0000

    Tracks have different colors

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -4,7 +4,7 @@ import EditablePolyline from './EditablePolyline.jsx'
 
 const max = (a,b) => a>=b?a:b
 const min = (a,b) => a<=b?a:b
-const colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00']
+const COLORS = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00']
 
 const LeafletMap = ({tracks}) => {
   var bounds = [{lat: Infinity, lon: Infinity}, {lat:-Infinity, lon:-Infinity}]
@@ -17,7 +17,7 @@ const LeafletMap = ({tracks}) => {
       bounds[1].lat = max(bounds[1].lat, elm.lat)
       bounds[1].lon = max(bounds[1].lon, elm.lon)
     })
-    return (<EditablePolyline positions={t} key={i} />)
+    return (<EditablePolyline positions={t} color={COLORS[i%COLORS.length]} key={i} />)
   })
 
   if (elements.length === 0) {
