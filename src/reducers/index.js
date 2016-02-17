f6fb12f612a74b5e85d15c30ed4b42b18e1d26d6
Rui Gil <ruipgil@gmail.com>
2016-02-17 15:00:50 +0000

    Fix removing segment when track has multiple segments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -83,13 +83,13 @@ const tracks = (state = [], action) => {
       segmentE.points.splice(action.index, 0, pointA)
       return nextStateE
     case 'REMOVE_SEGMENT':
-      // Bug when removing from multiple
       let track = state.map((track) => track.segments.find((s) => s.id === action.segmentId) ? track : null).find((x) => !!x)
       let stateF = [...state]
       if (track.segments.length === 1) {
         stateF.splice(stateF.indexOf(track), 1)
       } else {
-        stateF.segments.splice(stateF.segments.indexOf(track.segment.find((s) => s.id === action.segmentId)), 1)
+        let ix = track.segments.indexOf(track.segments.find((s) => s.id === action.segmentId))
+        track.segments.splice(ix, 1)
       }
       return stateF
     case 'SPLIT_SEGMENT':
