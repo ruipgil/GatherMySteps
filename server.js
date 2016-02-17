f5647f68a79d666b64cf3143808f92d31399de72
Rui Gil <ruipgil@gmail.com>
2016-02-17 00:29:30 +0000

    Change server port to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./webpack.config')
 
 var express = require('express')
 var app = new (express)()
-var port = 3000
+var port = 8080
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
@@ -13,10 +13,10 @@ app.use(webpackHotMiddleware(compiler))
 
 app.use(express.static('build'))
 
-app.listen(port, function(error) {
+app.listen(port, function (error) {
   if (error) {
     console.error(error)
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
   }
 })
